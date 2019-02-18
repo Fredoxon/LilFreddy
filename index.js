255a47cf17e1d34172aa4f0d9720ed69b0710abb
Florian <florian@raaab.de>
2019-02-18 13:12:00 +0100

    xpgain aktualisiert

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const config = require("./config.json");
 // config.token beinhaltet das Token
 // config.prefix beinhaltet den Prefix
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : config.host,
+  port     : '11234',
+  user     : config.user,
+  password : config.password,
+  database : 'phpMyAdmin',
+  charset : 'utf8mb4'
+});
+
 client.on("ready", () => {
   // Event, das startet, wenn der Bot sich erfolgreich eingeloggt hat
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
@@ -38,7 +48,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   // Bewirkt, dass der Bot nicht auf andere Bots antwortet (=> Verhindert Botception)
 
-  xpGain = Math.floor(Math.random() * (xpRange + 1)) + xpRangeStart;
+  xpGain = Math.floor(Math.random() * (xpRange + 1)) + parseInt(xpRangeStart, 10);
   console.log(`${xpGain} ${xpRange} ${xpRangeStart} \n`)
   // Willkür des tatsächlichen Erfahrungszuweisungsbetrags
   message.reply(xpGain);
@@ -52,8 +62,8 @@ client.on("message", async message => {
   console.log(`${args.length}`);
 
   if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    // Berechnet den Ping zwischen dem Senden und dem Bearbeiten einer Nachricht
+    // Der zweite Ping ist die durchschnittliche Latenz zwischen Bot und WebSocket Server
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
@@ -61,80 +71,22 @@ client.on("message", async message => {
   if(command === "xpgain") {
     xpRangeStart = args[0];
     xpRange = args[1] - args[0];
-    xpEnd = xpRange + xpRangeStart;
+    xpEnd = parseInt(xpRange, 10) + parseInt(xpRangeStart, 10);
     console.log(`${xpRangeStart} ${xpRange} ${xpEnd}`);
     message.reply(`Experience Range set from ${xpRangeStart} to ${xpEnd}`);
   }
-
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-  
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-  
-  if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
   
   if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
+    // Entfernt bis zu 100 Nachrichten eine Nutzers
     
-    // get the delete count, as an actual number.
+    // Parsing der Nummer an Nachrichten, die entfernt werden sollen
     const deleteCount = parseInt(args[0], 10);
     
-    // Ooooh nice, combined conditions. <3
+    // Bedingungsstellung
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
-    // So we get our messages, and delete them. Simple enough, right?
+    // Entfernen der Nachrichten
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
